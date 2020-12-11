1035578d222965cfa0f062625fae20562ed83e93
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-12 01:34:19 +0200

    initState only once, build ran whenever setState() is invoked.

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -6,8 +6,19 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("Init state function ran");
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("Build state function ran");
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -16,7 +27,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Text('Choose location screen'),
+      body: RaisedButton(
+        onPressed: (){
+          setState(() {
+            counter++;
+          });
+        },
+        child: Text('Counter is ${counter}'),
+      ),
     );
   }
 }

8bc7e16b1ed9fbd7474e3c8ddcdc8654c6ea5618
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-12 00:55:17 +0200

    pages or activities in android

---
+++ world_time/android/app/src/main/kotlin/com/ngelemar/world_time/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ngelemar.world_time
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ world_time/lib/pages/choose_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CooseLocation extends StatefulWidget {
+  @override
+  _CooseLocationState createState() => _CooseLocationState();
+}
+
+class _CooseLocationState extends State<CooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Choose location screen'),
+    );
+  }
+}
